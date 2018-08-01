de8de143ff776802521ab8478e87c57752c18c35
lijialiang <lijialiang@banggood.com>
2018-08-01 18:33:58 +0800

    添加filter获取sessionID

--- src/test/java/junitdemo/TestAuth.java
+++ src/test/java/junitdemo/TestAuth.java
@@ -3,6 +3,7 @@ package junitdemo;
 
 import io.restassured.RestAssured;
 import io.restassured.config.SessionConfig;
+import io.restassured.filter.session.SessionFilter;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -10,10 +11,10 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class TestAuth {
-    @BeforeClass
-    public static void setup(){
-        RestAssured.proxy("192.168.163.3", 8081);
-    }
+//    @BeforeClass
+//    public static void setup(){
+//        RestAssured.proxy("192.168.163.3", 8081);
+//    }
 
     @Test
     public void testAuthBasic(){
@@ -25,14 +26,18 @@ public class TestAuth {
     @Test
     public void testJenkinsLogin(){
 
-        RestAssured.config().sessionConfig(
-                new SessionConfig().sessionIdName(" JSESSIONID"));
+        RestAssured.config  = RestAssured.config().sessionConfig(
+                new SessionConfig().sessionIdName("JSESSIONID"));
+
+        SessionFilter sessionFilter = new SessionFilter();
 
         given()
+                .filter(sessionFilter)
                 .queryParam("Submit","%E7%99%BB%E5%BD%95")
                 .queryParam("j_username","ll")
                 .queryParam("j_password","ll")
                 .queryParam("from","%2Fjenkins%2F")
+
         .when()
                 .log().all()
                 .post("http://192.168.163.3:8080/jenkins/j_acegi_security_check").prettyPeek()
@@ -40,8 +45,15 @@ public class TestAuth {
                 .log().all()
                 .statusCode(302);
 
+        given()
+//                .sessionId("79BFCA8B54B0EF17761CD7753DA2B327")
+                .filter(sessionFilter)
+        .when()
+                .log().all()
+                .get("http://192.168.163.3:8080/jenkins/").prettyPeek()
+        .then()
+                .statusCode(200);
+
     }
-
-
 
 }
