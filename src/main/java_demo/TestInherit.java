591a42b6b25004fde6b05fe2961e476171f1f517
lijialiang <lijialiang@banggood.com>
2018-12-12 11:03:08 +0800

    添加注释

--- src/main/java_demo/TestInherit.java
+++ src/main/java_demo/TestInherit.java
@@ -2,6 +2,9 @@
 public class TestInherit {
 
     public static void main(String[] args) {
+
+
+//        继承的写法，继承表达的“是”的关系。狼是动物
 //        Bird b = new Bird();
 //        b.breath();
 //        b.fly();
@@ -9,17 +12,20 @@ public class TestInherit {
 //        w.breath();
 //        w.run();
 
+
+
+//        复用的写法，组合表达的是“有”的关系。人由手臂组成
+//        显式创建被嵌套对象
         Animal a1 = new Animal();
         Bird b = new Bird(a1);
         b.breath();
         b.fly();
+        //        显式创建被嵌套对象
         Animal a2 = new Animal();
         Wolf w = new Wolf(a2);
         w.breath();
         w.run();
 
-
     }
-
 
 }
