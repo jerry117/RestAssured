ab60a2eee30693f550c8da365a8a1e7dfa88a57f
lijialiang <lijialiang@banggood.com>
2018-12-14 09:38:13 +0800

    demo

---
+++ src/main/java_demo/TestImmutableString.java
@@ -0,0 +1,15 @@
+public class TestImmutableString {
+
+    public static void main(String[] args) {
+        String str1 = new String("hello");
+        String str2 = new String("hello");
+
+
+        System.out.println(str1 == str2);
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.hashCode());
+        System.out.println(str2.hashCode());
+
+    }
+}
+
