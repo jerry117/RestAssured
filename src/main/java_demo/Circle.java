bf4aee96dc0da5fc075f9cda4c88a78154367c3e
lijialiang <lijialiang@banggood.com>
2018-12-14 11:20:00 +0800

    抽象类

---
+++ src/main/java_demo/Circle.java
@@ -0,0 +1,32 @@
+public class Circle extends Shape {
+
+    private double radius;
+    public Circle (String color , double radius){
+
+        super(color);
+        this.radius = radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public String getType(){
+        return getColor()+ "圆形";
+    }
+
+
+    public static void main(String[] args) {
+        Shape s1 = new Triangle("sdf", 3,4,5);
+        Shape s2 = new Circle("adjfl", 7);
+        System.out.println(s1.getType());
+        System.out.println(s1.calPerimeter());
+        System.out.println(s2.getType());
+        System.out.println(s2.calPerimeter());
+    }
+}
---
+++ src/main/java_demo/Shape.java
@@ -0,0 +1,32 @@
+public abstract class Shape {
+
+
+    {
+        System.out.println("初始化");
+    }
+
+    private String color;
+
+    public abstract double calPerimeter();
+
+    public abstract String getType();
+
+    public Shape(){}
+    public Shape(String color){
+        System.out.println("执行shape构造器");
+        this.color = color;
+
+    }
+
+    public void setColor(String color){
+        this.color = color;
+
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+
+
+}
