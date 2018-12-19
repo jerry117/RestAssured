992891e3d79a0f426d19a33d65313c9df8445e15
lijialiang <lijialiang@banggood.com>
2018-12-19 10:47:10 +0800

    set demo

---
+++ src/main/java_demo/TestHashSet.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+
+class A1{
+    @Override
+    public boolean equals(Object obj) {
+        return true;
+    }
+}
+class B{
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+}
+
+class C{
+    @Override
+    public int hashCode() {
+        return 2;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return true;
+    }
+}
+
+public class TestHashSet {
+    public static void main(String[] args) {
+        HashSet books = new HashSet();
+        books.add(new A1());
+        books.add(new A1());
+        books.add(new B());
+        books.add(new B());
+        books.add(new C());
+        books.add(new C());
+        System.out.println(books);
+    }
+}
